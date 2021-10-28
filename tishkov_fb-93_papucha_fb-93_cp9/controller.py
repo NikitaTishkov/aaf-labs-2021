513a292e9f917adeec8ddf4b2484c40a590836d4
NikitaTishkov <nikitatishkov77@gmail.com>
2021-10-28 13:34:09 +0300

    Add PRINT_INDEX command function; Add new condition parts for new command

--- tishkov_fb-93_papucha_fb-93_cp9/controller.py
+++ tishkov_fb-93_papucha_fb-93_cp9/controller.py
@@ -9,50 +9,50 @@ class Controller:
         """Show information about all commands 
         or about specific one """
         if command == "CREATE":
-            #TODO: Write CREATE command description
             print("CREATE - creating a new collection")
             print("Example:")
             print("CREATE wiki_articles; ")
         elif command == "INSERT":
-            #TODO: Write INSERT command description
             print("INSERT - add a new document to the collection")
             print("Example:")
             print("INSERT wiki_articles “The word 'algorithm' has its roots in Latinizing the ... ”;")
         elif command == "SEARCH":
-            #TODO: Write SELECT command description
             print("SEARCH - search for documents in the collection")
             print("Example:")
             print("SEARCH wiki_articles;")
             print("SEARCH wiki_articles WHERE “algorithm”; ('algorithm')")
             print("SEARCH wiki_articles WHERE “haa” - “haz”; (has)")
             print("SEARCH wiki_articles WHERE “has” <2> “roots”; (its)")
+        elif command == "PRINT_INDEX":
+            #TODO: Add PRINT_INDEX command description
+            pass
         elif command == "ALL":
-            #TODO: Show all commands description 
-            # Maybe it`s better to use recursion
-            controller = Controller()
-            controller.info_command("CREATE")
-            controller.info_command("INSERT")
-            controller.info_command("SEARCH")
+            self.info_command("CREATE")
+            self.info_command("INSERT")
+            self.info_command("SEARCH")
 
     def parse_code(self, text):
         """Splits string into commands and parse them"""
         parsed_commands = text.split("; ")
-        print(parsed_commands)
+        #print(parsed_commands)
         for command in parsed_commands:
             self.parse_command(command)
         
 
     def parse_command(self, command):
         """Recognize specific command and call it"""
-        if command.split()[0].lower() == "CREATE":
+        if command.split()[0].upper() == "CREATE":
             #call CREATE request
             self.request.CREATE(command.split()[1])
-        elif command.split()[0].lower() == "INSERT":
+        elif command.split()[0].upper() == "INSERT":
             #TODO: call INSERT request
             print("INSERT")
-        elif command.split()[0].lower() == "SEARCH":
+        elif command.split()[0].upper() == "SEARCH":
             #TODO: call SEARCH request
             print("SEARCH")
+        elif command.split()[0].upper() == "PRINT_INDEX":
+            #TODO: call PRINT_INDEX request
+            print("PRINT_INDEX")
         else:
             print("Wrong command, try again!")
             #TODO: Show commands info
@@ -84,8 +84,6 @@ class Request:
 
         try:
             index_table = open(collection_name + "/indexes.json", "w")
-            # Add first description fields to json file
-            #TODO: Verify this solution with teacher
             index_table.close()
         except OSError as error:
             print("Could not open/read file")
@@ -97,16 +95,23 @@ class Request:
     def INSERT(self, collection_name, doc_str):
         """Insert document with value /doc_str/ 
         to collection /collection_name/"""
-
-    def SEARCH(self):
         #TODO: Complete functionality
-        print()
+    def SEARCH(self, collection_name):
+        """Full text search in specific 
+        collection /collection_name/"""
+        #TODO: Complete functionality
+
+    def PRINT_INDEX(self, collection_name):
+        """Show word-indexes pairs for specific 
+        collection /collection_name/"""
+        #TODO: Complete functionality
+        pass
 
 def info_global():
     """Show app descrition 
     (The goal and the basic instructions)"""
     #TODO: Add full description for application
-    print("HEllo!")
+    pass
 
 def main():
     #Example:
