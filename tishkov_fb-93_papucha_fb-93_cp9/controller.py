f0884b2befbe975a88e2c7b0152e9c27983b2019
Nikita Tishkov <nikitatishkov77@gmail.com>
2021-11-26 23:18:42 +0200

    Small refactoring

--- tishkov_fb-93_papucha_fb-93_cp9/controller.py
+++ tishkov_fb-93_papucha_fb-93_cp9/controller.py
@@ -2,6 +2,8 @@
 import os
 import json
 from pathlib import Path
+
+
 class Controller:
     def __init__(self):
         self.request = Request()
@@ -44,29 +46,28 @@ class Controller:
         if text[-1] == ';':
             text = text[:text.rfind(';')]
         parsed_commands = text.split("; ")
-        #print(parsed_commands)
         for command in parsed_commands:
             self.parse_command(command)
-        
 
     def parse_command(self, command):
         """Recognize specific command and call it"""
         if command.split()[0].upper() == "CREATE":
-            #call CREATE request
+            # call CREATE request
             self.request.CREATE(command.split()[1])
         elif command.split()[0].upper() == "INSERT":
             doc_str = command[command.find('"') + 1:command.rfind('"') - 1]
             self.request.INSERT(command.split()[1], doc_str)
         elif command.split()[0].upper() == "SEARCH":
-            #TODO: call SEARCH request
+            # TODO: call SEARCH request
             print("SEARCH")
         elif command.split()[0].upper() == "PRINT_INDEX":
-            #TODO: call PRINT_INDEX request
+            # TODO: call PRINT_INDEX request
             print("PRINT_INDEX")
         else:
             print("Wrong command, try again!")
-            #TODO: Show commands info
-         
+            # TODO: Show commands info
+
+
 class Request:
 
     def __init__(self):
@@ -134,12 +135,12 @@ class Request:
 
         i = 0
         for word in doc_str.split():
-            words_addr_list = []
-            if index_table.get(word) != None:
-                if index_table.get(word).get(doc_counter[collection_name]) != None:
-                    words_addr_list = index_table.get(word).get(doc_counter[collection_name])
-            words_addr_list.append(i)
-            index_table.update({word: {doc_counter[collection_name]:words_addr_list}})
+            words_adr_list = list()
+            if index_table.get(word) is not None:
+                if index_table.get(word).get(doc_counter[collection_name]) is not None:
+                    words_adr_list = index_table.get(word).get(doc_counter[collection_name])
+            words_adr_list.append(i)
+            index_table.update({word: {doc_counter[collection_name]:words_adr_list}})
             i = i + 1
         index_table_file = open(collection_name + "/indexes.json", "w")
         index_table_file.write(json.dumps(index_table, indent=4))
@@ -157,22 +158,35 @@ class Request:
     def SEARCH(self, collection_name):
         """Full text search in specific 
         collection /collection_name/"""
-        #TODO: Complete functionality
+        # TODO: Complete functionality
 
     def PRINT_INDEX(self, collection_name):
         """Show word-indexes pairs for specific 
         collection /collection_name/"""
-        #TODO: Complete functionality
+        # TODO: Complete functionality
         pass
 
+
 def info_global():
-    print("Collection of text documents with full-text search./n Our program allows you to search for words in several documents at once. The program analyzes the documents that are attached to the collections, selects individual words from them and saves a list of items where the word occurs within the document./n This can be convenient for accountants because they have a lot of documents that need to be processed./n This program has four commands that will help the user to use:/n CREATE collection_name; - creating a new collection called collection_name./n INSERT collection_name “value”; - adding a new document to the collection collection_name./n PRINT_INDEX collection_name; - display of the internal structure of the inverted index built for the collection collection_name./n SEARCH collection_name [WHERE query]; - search for documents in the collection collection_name.")
+    print("Collection of text documents with full-text search. ")
+    print("Our program allows you to search for words in several documents at once. "
+          "The program analyzes the documents that are attached to the collections,"
+          " selects individual words from them and saves a list of items where the word occurs within the document.")
+    print("This can be convenient for accountants because they have a lot of documents that need to be processed.")
+    print("This program has four commands that will help the user to use:")
+    print("CREATE collection_name; - creating a new collection called collection_name.")
+    print("INSERT collection_name “value”; - adding a new document to the collection collection_name.")
+    print("PRINT_INDEX collection_name; - display of the internal structure of the inverted index built for the "
+          "collection collection_name.")
+    print("SEARCH collection_name [WHERE query]; - search for documents in the collection collection_name.")
+
 
 def main():
-    #Example:
+    # Example:
     controller = Controller()
     s = input()
     controller.parse_code(s)
+
 
 if __name__ == '__main__':
     main()
